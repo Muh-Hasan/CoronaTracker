f9149776b7d0da182e8ddbcfaac2069ee922dca6
Muhammad Hasan <muh.hasan.git@gmail.com>
2020-06-21 10:05:04 +0500

    final commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <h1 className={styles.head}>COIVD - 19</h1>        
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
+}
+
+export default App;
